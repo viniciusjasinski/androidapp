7292db6bfbacc5aa122c2759b8558ba043d81f63
viniciusjasinski <vinicius.jasinski@gmail.com>
2021-07-28 08:50:24 -0300

    Segundo commit

---
+++ app/src/main/java/com/example/primeiroprojetoandroid/classes/Cobra.kt
@@ -0,0 +1,4 @@
+package com.example.primeiroprojetoandroid.classes
+
+class Cobra {
+}
---
+++ app/src/main/java/com/example/primeiroprojetoandroid/classes/Veiculo.kt
@@ -0,0 +1,11 @@
+package com.example.primeiroprojetoandroid.classes
+
+class Veiculo(
+    private val motor : String
+) {
+
+    fun verificaMotor(){
+        println(motor)
+    }
+
+}
